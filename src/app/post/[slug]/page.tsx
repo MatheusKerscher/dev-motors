34d8b6718a0c96d8f7924df77730b4aa2e177fb4
MatheusKerscher <kerschermatheus12@gmail.com>
2025-09-12 20:32:03 -0300

    Applying React Suspense API

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,10 +1,9 @@
+import { Suspense } from "react";
 import type { Metadata } from "next";
-import { MessageCircle } from "lucide-react";
 
 import { getPostBySlug } from "@/utils/actions/get-data";
-import Article from "@/components/article";
-import Hero from "@/components/hero";
-import Container from "@/components/container";
+import Content from "./components/content";
+import LoadingPost from "./components/loadingPost";
 
 type PostProps = {
   params: {
@@ -59,35 +58,12 @@ export async function generateMetadata({
 
 const Post = async ({ params }: PostProps) => {
   const { slug } = await params;
-  const postProps = (await getPostBySlug(slug)).objects[0];
 
   return (
     <main>
-      <Hero
-        title={postProps.metadata.hero.title}
-        bannerUrl={postProps.metadata.hero.banner.url}
-        buttonLabel={postProps.metadata.hero.button_label}
-        buttonUrl={postProps.metadata.hero.button_url}
-        buttonBackground={postProps.metadata.hero.button_background}
-        buttonColor={postProps.metadata.hero.button_text_color}
-        icon={
-          <MessageCircle
-            color={postProps.metadata.hero.button_text_color}
-            size={20}
-          />
-        }
-      />
-
-      <Container>
-        <Article
-          title={postProps.metadata.post.title}
-          description={postProps.metadata.post.description}
-          imgUrl={postProps.metadata.post.banner.url}
-          showButton={postProps.metadata.post.show_button}
-          buttonLabel={postProps.metadata.post.button_label}
-          buttonUrl={postProps.metadata.post.button_url}
-        />
-      </Container>
+      <Suspense fallback={<LoadingPost />}>
+        <Content slug={slug} />
+      </Suspense>
     </main>
   );
 };
